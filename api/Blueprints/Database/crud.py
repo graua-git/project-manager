9effe5e82eac8da22e8148e6707d76951597b4c0
graua-git <graua@oregonstate.edu>
2023-08-30 14:06:10 -0700

    Read CRUD operation

--- api/Blueprints/Database/crud.py
+++ api/Blueprints/Database/crud.py
@@ -28,6 +28,25 @@ def create(entry: dict, table: str) -> dict:
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+def read(query_title: str, quantity: str = 'all') -> list:
+    """
+    SELECT Query
+    query_title: Comment labeling the query in dml.sql
+    returns: list of lists representing table
+    quantity: "one" or "all"
+    """
+    query = get_query(query_title)
+    try:
+        # Query Database
+        cursor.execute(query)
+        table = cursor.fetchall()
+        if quantity == "one":
+            return table[0]
+        else:
+            return table
+    except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
 def get_query(query_title: str) -> str:
     """
     Gets query from dml.sql
