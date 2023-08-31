fde60e12aa06738860557d067f61d05aad00f2af
graua-git <graua@oregonstate.edu>
2023-08-31 14:06:13 -0700

    registered users bp

--- api/main.py
+++ api/main.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 
+from Blueprints.users import users_bp
+
 app = Flask(__name__)
 
 # Add Access for web app
 CORS(app)
 CORS(app, resources={r"/*": {'origins': 'http://localhost:3000'}})
 
-@app.route('/')
-def default():
-    return "Flask API"
+# Register Blueprints
+app.register_blueprint(users_bp, url_prefix='/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
