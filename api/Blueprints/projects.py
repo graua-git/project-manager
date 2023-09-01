a0e64369372fa55132af1d42b797510b857d0b50
graua-git <graua@oregonstate.edu>
2023-08-31 21:31:48 -0700

    projects endpoints

---
+++ api/Blueprints/projects.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+
+from Authentication.authentication import generate_token, validate_token
+from Database.crud import create, read, update, delete
+
+projects_bp = Blueprint('projects', __name__)
+
+@projects_bp.route('/create', methods=['POST'])
+def create_project():
+    """
+    Creates project, requires info from json
+    """
+    return create(request.get_json(), "Projects")
+
+@projects_bp.route('/read-all', methods=['GET'])
+def read_projects():
+    """
+    Returns the following table
+    project_id | email | first_name | last_name
+    """
+    return read("Read Projects")
+
+@projects_bp.route('/read-one', methods=['GET'])
+def read_projects():
+    """
+    Returns the following table
+    project_id | email | first_name | last_name
+    """
+    return read("Read Projects", "one")
--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from Blueprints.users import users_bp
+from Blueprints.projects import projects_bp
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ CORS(app, resources={r"/*": {'origins': 'http://localhost:3000'}})
 
 # Register Blueprints
 app.register_blueprint(users_bp, url_prefix='/users')
+app.register_blueprint(projects_bp, url_prefix='/projects')
 
 if __name__ == '__main__':
     app.run(debug=True)
