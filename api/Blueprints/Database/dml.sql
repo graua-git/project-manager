f4f43eb4f9e14e522cadfb6d8647f7bed72bf79d
graua-git <graua@oregonstate.edu>
2023-09-06 09:56:27 -0700

    Read My Projects query

--- api/Blueprints/Database/dml.sql
+++ api/Blueprints/Database/dml.sql
@@ -9,6 +9,17 @@ DELETE FROM Users WHERE user_id = {id};
 -- Read Projects
 SELECT * FROM Projects;
 
+-- Read My Projects
+SELECT name, owner
+FROM Projects
+JOIN Memberships ON Memberships.project = Projects.project_id
+JOIN Users ON Users.user_id = Memberships.user
+JOIN (SELECT project, creator, CONCAT(Users.first_name, ' ', Users.last_name) as owner
+FROM Memberships
+JOIN Users ON Users.user_id = Memberships.user
+WHERE creator = 1) as Creators ON Projects.project_id = Creators.project
+WHERE Users.user_id = {user_id};
+
 -- Memberships
 -- Read Memberships
 SELECT membership_id, CONCAT(first_name, ' ', last_name) AS participant_name, email, name AS project, creator
