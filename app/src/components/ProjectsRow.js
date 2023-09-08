0053fce9b831aa1d38fc9ba9a5c7d40f8eccc7d1
graua-git <graua@oregonstate.edu>
2023-09-08 11:34:20 -0700

    project row navigates to project page

--- app/src/components/ProjectsRow.js
+++ app/src/components/ProjectsRow.js
@@ -1,6 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function ProjectsRow({project, removeProject}) {
+    let navigate = useNavigate();
+
+    const handleProjectNavigate = () => {
+        console.log(project.project_id)
+        navigate(`/project/${project.project_id}`)
+    }
+
     return (
-        <tr>
+        <tr onClick={handleProjectNavigate}>
             <td>{project.name}</td>
             <td>{project.owner}</td>
         </tr>
